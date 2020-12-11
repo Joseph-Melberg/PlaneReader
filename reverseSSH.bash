cc8819ae932e231dec936d1547767149cff7da8d
Joseph-Melberg <vtechdelete@gmail.com>
2020-12-11 15:34:53 +0000

    Cleanup

--- reverseSSH.bash
+++ reverseSSH.bash
@@ -6,7 +6,7 @@ while true
 do
 ./Log.py INFO gen reverseSSH  "Tunnel open attempted: $1:localhost:22"
 ssh -N -R $1:localhost:22 jump@jump.centurionx.net
-./Log.py INFO gen Homebase reverseSSH  "Tunnel closed" 
+./Log.py INFO gen reverseSSH  "Tunnel closed" 
 sleep 60
 done
 
