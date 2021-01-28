8c63b7e3215fd94ff86e5b52c362c229674010c3
Joseph-Melberg <dasbasdo@gmail.com>
2021-01-28 14:06:35 -0600

    Update reverseSSH.bash
    This is kinda stupid to do because it opens up every pi to a set of attacks, but I need to be able to freely change which server is acting as jump and it is better to be able to do this on the fly than to have to go physically visit each pi and do an update

--- reverseSSH.bash
+++ reverseSSH.bash
@@ -4,9 +4,7 @@ sleep 60
 echo "Forming Connection"
 while true
 do
-./Log.py INFO gen reverseSSH  "Tunnel open attempted: $1:localhost:22"
-ssh -N -R $1:localhost:22 jump@jump.centurionx.net
-./Log.py INFO gen reverseSSH  "Tunnel closed" 
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -N -R $1:localhost:22 jump@jump.centurionx.net
 sleep 60
 done
 
